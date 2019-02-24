2e1ec7f33bde64c98b0d0920729683b5a5c05a63
bretello <bretello@distruzione.org>
2019-02-24 17:28:51 +0100

    automatic download of today's paper, proper sort

--- ilfoglio.sh
+++ ilfoglio.sh
@@ -4,8 +4,9 @@
 
 ###### GET PARAMETERS #######
 if [[ $# -eq 0 ]]; then
-    echo "Assuming today's date: $(date +"%y-%m-%d")"
-    DATE=$(date +"%y%m%d")
+    echo "Scarico il giornale di oggi: $(date +"%d-%m-%Y")"
+    DATE=$(date +"%Y%m%d")
+    YEAR=$(date +"%Y")
 else
     while getopts y:m:d:w: option; do
         case "${option}" in
@@ -18,37 +19,37 @@ else
 fi
 
 weekday=$(date -d $DATE +"%u")
-if [[ $weekday -eq 6  || $weekday -eq 7 ]]; then  # 6 and 7 are Saturday and Sunday
+if [[ $weekday -eq 6 || $weekday -eq 7 ]]; then  # 6 and 7 are Saturday and Sunday
     WEEKEND='true'
 fi
 ############################
 
-if  [[ -z "$DAY" ]] || [[ -z "$MONTH" ]] || [[ -z "$YEAR" ]] ; then
+if  [[ -z $DATE ]] ; then
     echo "Usage: ./ilfoglio.sh [-y YYYY -m MM -d DD]"
     echo "Scarica il giornale di oggi se non vengono dati argomenti."
 else
-
-    mkdir $DATE
-
-    download () {
-        if [ "$WEEKEND" = "true" ]; then
-            curl -s -o ${DATE}/${1}.jpg https://edicola.ilfoglio.it/ilfoglio/books/weekend/$YEAR/${DATE}weekend/images/zoompages/Zoom-$1.jpg
-        else
-            curl -s -o ${DATE}/${1}.jpg https://edicola.ilfoglio.it/ilfoglio/books/ilfoglio/$YEAR/${DATE}ilfoglio/images/zoompages/Zoom-$1.jpg
-        fi
-    }
-
+    download_dir="ilfoglio_$DATE"
+    mkdir -p "$download_dir"
+    cd $download_dir
     echo "Downloading.."
-
-    for i in {1..30}
-    do
-        download $i &
+    if [ "$WEEKEND" = "true" ]; then
+        function download_page(){
+            curl -s -o "$(printf "%02d" $1).jpg" "https://edicola.ilfoglio.it/ilfoglio/books/weekend/$YEAR/${DATE}weekend/images/zoompages/Zoom-$1.jpg"
+        }
+    else
+        function download_page(){
+            curl -s -o "$(printf "%02d" $1).jpg" "https://edicola.ilfoglio.it/ilfoglio/books/ilfoglio/$YEAR/${DATE}ilfoglio/images/zoompages/Zoom-$1.jpg"
+        }
+    fi
+    for i in `seq 1 30`; do
+        download_page $i &
     done
-
     wait $(jobs -p)
-    echo "Download completed. Files saved in ${DATE}/ ."
 
-    echo "Cleaning up.."
-    find ./${DATE} -exec file {} \; | grep HTML | cut -d: -f1 | xargs rm
+    echo "Download completed. Files saved in $download_dir."
+    cd ..
+
+    #echo "Cleaning up.."
+    find ./$download_dir -exec file {} \; | grep HTML | cut -d: -f1 | xargs rm 2>/dev/null
     echo "Done."
 fi
